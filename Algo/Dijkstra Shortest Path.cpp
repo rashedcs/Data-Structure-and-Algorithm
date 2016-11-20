791a3564c0d77bcf027bff8326def72fdd160223
rashedcs <rashedul3500@gmail.com>
2016-11-20 11:44:35 +0600

    Update Dijkstra Shortest Path.cpp

--- Algo/Dijkstra Shortest Path.cpp
+++ Algo/Dijkstra Shortest Path.cpp
@@ -3,26 +3,26 @@
 #define infinity 999999999999999999
 using namespace std;
 
-long long int n,m,u,v,w,i,d[1000000],dir[1000000];
+long long int n,m,u,v,w,d[1000000],dir[1000000];
 vector<pii>G[1000000];
 priority_queue<pii,vector<pii>,greater<pii> >q;
-vector<int>print;
+vector<int>path;
 
 
 int main()
 {
-    int tc, start;
+     int tc, start;
 
-    cin>>n>>m;
+     cin>>n>>m;
 
      for(int i=0; i<m; i++)
-    {
+     {
         cin>>u>>v>>w;
         G[u].push_back(pii(w,v));
         G[v].push_back(pii(w,u));
-    }
+     }
 
-    for(i=1; i<=n; i++)
+    for(int i=1; i<=n; i++)
     {
         d[i]=infinity;
         dir[i]=-1;
@@ -32,11 +32,12 @@ int main()
 
     q.push(pii(0,start));
     d[start]=0;
+
     while(!q.empty())
     {
         u=q.top().second;
         q.pop();
-        for(i=0; i<G[u].size();i++)
+        for(int i=0; i<G[u].size();i++)
         {
             v=G[u][i].second;
             w=G[u][i].first;
@@ -48,22 +49,29 @@ int main()
                 q.push(pii(d[v],v));
             }
         }
+     }
 
+
+    if(dir[n]==-1)
+    {
+       cout<<"-1"<<endl;
     }
-    
-    if(dir[n]!=-1)
+
+    else
     {
         u=n;
         while(u!=-1)
         {
-            print.push_back((u));
+            path.push_back((u));
             u=dir[u];
         }
-    reverse(print.begin(),print.end());
-    for(i=0;i<print.size();i++)
-        cout<<print[i]<<" ";
-    cout<<endl;
+
+        for(int i=path.size()-1; i>=0; i--)
+        {
+           cout<<path[i]<<" ";
+        }
+        cout<<endl;
     }
-    else cout<<"-1"<<endl;
+
     return 0;
 }
