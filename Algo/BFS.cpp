a24dc43c3ef5493c0c0839cab2f16475e7bb205f
rashedcs <rashedul3500@gmail.com>
2016-11-30 02:15:21 +0600

    Update BFS.cpp

--- Algo/BFS.cpp
+++ Algo/BFS.cpp
@@ -66,6 +66,71 @@ int main()
 
 
 
+//Best Way :
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>nodes[1000];
+
+void bfs(int start, int vertex)
+{
+
+     queue<int>que;
+
+     bool visited[vertex+1];
+
+     for(int i=0; i<vertex; i++)
+     {
+        visited[i]=false;
+     }
+
+         //bool *visited = new bool[V];
+	//bool *visited = new bool[V];
+     que.push(start);
+     visited[start]=true;
+
+     while(!que.empty())
+     {
+		 int u = que.front();
+		 cout<<u<<" ";
+		 que.pop();
+         for(int i=0;i<nodes[u].size();i++)
+         {
+            int v = nodes[u][i];
+            if(visited[v]==false)
+            {
+                visited[v]=true;
+                que.push(v);
+            }
+         }
+     }
+
+}
+
+
+
+int main()
+{
+
+        int start, vertex, edges,a,b;
+
+        cout<<"Enter the no of vertex and edges"<<endl;
+	    cin>>vertex>>edges;
+	    for(int i=0;i<edges;i++)
+	    {
+		  cin>>a>>b;
+		  nodes[a].push_back(b);
+		  //nodes[b].push_back(a);
+	    }
+
+       cout<<"\nEnter the starting node"<<endl;
+       cin>>start;
+       bfs(start, vertex);
+
+       cout<<endl;
+       return 0;
+
+}
 
 
 
