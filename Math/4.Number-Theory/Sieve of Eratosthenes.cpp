d530be89ae330281549a8f7c2410b5fb4c319457
rashedcs <rashedul3500@gmail.com>
2016-12-08 20:54:19 +0600

    Update Sieve of Eratosthenes.cpp

--- Math/4.Number-Theory/Sieve of Eratosthenes.cpp
+++ Math/4.Number-Theory/Sieve of Eratosthenes.cpp
@@ -4,7 +4,6 @@ void sieve(int num)
     long long sum = 0;
     bool prime[num+1];
     memset(prime, true, sizeof(prime));
-    
     for (int p = 2; p*p<=num; p++)
     {
         if (prime[p] == true)
