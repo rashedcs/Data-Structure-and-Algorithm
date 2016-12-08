e86d8bffa70b16cfb404de262cdebc503bebba6f
rashedcs <rashedul3500@gmail.com>
2016-12-08 20:53:35 +0600

    Update and rename num.cpp to Sieve of Eratosthenes.cpp

---
+++ Math/4.Number-Theory/Sieve of Eratosthenes.cpp
@@ -0,0 +1,26 @@
+// Sieve of Eratho
+void sieve(int num)
+{
+    long long sum = 0;
+    bool prime[num+1];
+    memset(prime, true, sizeof(prime));
+    
+    for (int p = 2; p*p<=num; p++)
+    {
+        if (prime[p] == true)
+        {
+            for(int i = p*2; i <= num; i = i + p)
+            prime[i] = false;
+        }
+    }
+    
+    for (int p = 2; p<=num; p++)
+    {
+        if (prime[p])
+        {
+            cout<<p<<" ";
+        }
+    }
+    
+    cout<<endl;
+}
