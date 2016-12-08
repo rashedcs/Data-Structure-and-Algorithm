1a0c137c6dc731191d02ad0d83dcf374a0ff3c45
rashedcs <rashedul3500@gmail.com>
2016-12-09 00:35:19 +0600

    Update EulerPhi.cpp

--- Math/4.Number-Theory/EulerPhi.cpp
+++ Math/4.Number-Theory/EulerPhi.cpp
@@ -5,9 +5,10 @@
 int phi(unsigned int n)
 {
     unsigned int result = 1;
-    for (int i=2; i < n; i++)
-        if (__gcd(i, n) == 1)
-            result++;
+    for (int i=2; i<n; i++)
+    {
+        if (__gcd(i, n) == 1)  result++;
+    }
     return result;
 }
 
