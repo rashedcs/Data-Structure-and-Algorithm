ca43d950e4cbc6a506707cec279f10487f746bab
rashedcs <rashedul3500@gmail.com>
2016-12-09 12:43:38 +0600

    Update Prime Factor.cpp

--- Math/4.Number-Theory/Prime Factor.cpp
+++ Math/4.Number-Theory/Prime Factor.cpp
@@ -1,11 +1,26 @@
 //Unique Prime Factor
-
+void PF(int n)
+{
+  for(int i=2; i<=n; i++)
+  {
+     int cnt = 0;
+     if(n%i==0)
+     {
+       while(n%i==0)
+       {
+         n = n/i;
+         cnt++;
+       }
+       cout<<i<<" ";
+    }
+  }
+}
 
 
 
 
 //Not unique Prime Factor
-void PrimeFact(ll num)
+void PF(ll num)
 {
     cout<<"1";
     for (ll i=2; i<=num; i++)
