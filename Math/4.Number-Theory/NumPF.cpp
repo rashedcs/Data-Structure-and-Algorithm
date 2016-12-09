3cc157b1ac6162fb0d8906582877e70c2204911d
rashedcs <rashedul3500@gmail.com>
2016-12-09 11:12:22 +0600

    Update NumPF.cpp

--- Math/4.Number-Theory/NumPF.cpp
+++ Math/4.Number-Theory/NumPF.cpp
@@ -14,3 +14,20 @@ int NumPrimeFactor(int n)
 
     return result;
 }
+
+
+void PF(int n)
+{
+  for(int i=2; i<=n; i++)
+  {
+    int cnt = 0;
+    if(n%i==0)
+    {
+      while(n%i==0)
+      {
+        n = n/i;
+      }
+      cout<<i<<" ";
+    }
+  }
+}
