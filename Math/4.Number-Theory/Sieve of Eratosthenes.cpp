e35b04b8b711835687cb3ad8f2210fc4ff8b8fe4
rashedcs <rashedul3500@gmail.com>
2016-12-10 22:18:50 +0600

    Update Sieve of Eratosthenes.cpp

--- Math/4.Number-Theory/Sieve of Eratosthenes.cpp
+++ Math/4.Number-Theory/Sieve of Eratosthenes.cpp
@@ -1,3 +1,54 @@
+//Sieve of Eratosthenes
+void sieve(int MAX)
+{
+    bool *prime = new bool[MAX];      //  http://ideone.com/RUdNpk this link use int instead of bool
+    memset(prime, 0, sizeof(prime));  // It is good to use int instead of bool 
+
+    for(int i=2; i*i<=MAX;i++)
+    {
+        if(prime[i]==0)
+        {
+           for(int j=2*i; j<=MAX; j+=i)
+           {
+             if(prime[j]==0)  prime[j]=i;
+           }
+        }
+    }
+
+    for (int p=2; p<=MAX; p++)
+    {
+        if (!prime[p])
+        {
+            cout<<p<<" ";
+        }
+    }
+    cout<<endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Sieve of Eratho Source: http://www.geeksforgeeks.org/sieve-of-eratosthenes/
 void sieve(int num)
 {
