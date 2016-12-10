f5971f4d11e9317797d27fb8d4394ea6d2e70706
rashedcs <rashedul3500@gmail.com>
2016-12-10 22:13:19 +0600

    Update sumDiv.cpp

--- Math/4.Number-Theory/sumDiv.cpp
+++ Math/4.Number-Theory/sumDiv.cpp
@@ -1,11 +1,22 @@
 //Naive Solution :
+int sumDivisor(int n)
+{
+   int  sum = 0;
+   for(int i=1; i<=n/2; i++)
+   {
+     if(n%i==0) sum += i;
+   }
+   return sum+n;
+}
+
+
+
 
 
 //Better Approach 
 int numDiv(int n)
 {
     int result = 1;   // Initialize result as 1
-
     for (int p=2; p*p<=n; p++)
     {
         int power = 0;
@@ -16,9 +27,6 @@ int numDiv(int n)
         }
         result *= (pow(p, power + 1.0) - 1) / (p-1);
     }
-
     if (n!=1)    result *= (pow(n,2.0) - 1) / (n-1);
-
     return result;
-
 }
