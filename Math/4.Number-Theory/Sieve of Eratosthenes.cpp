4b0ec45ce0ecd1b2ce5820c9caf50143b2bb9426
rashedcs <rashedul3500@gmail.com>
2016-12-12 21:46:19 +0600

    Update Sieve of Eratosthenes.cpp

--- Math/4.Number-Theory/Sieve of Eratosthenes.cpp
+++ Math/4.Number-Theory/Sieve of Eratosthenes.cpp
@@ -50,6 +50,40 @@ void sieve(int MAX)
 
 
 // Sieve of Eratho Source: http://www.geeksforgeeks.org/sieve-of-eratosthenes/
+void SieveOfEratosthenes(int n)
+{
+	// Create a boolean array "prime[0..n]" and initialize
+	// all entries it as true. A value in prime[i] will
+	// finally be false if i is Not a prime, else true.
+	bool prime[n+1];
+	memset(prime, false, sizeof(prime));
+
+	for (int p=2; p*p<=n;  p++)
+	{
+		// If prime[p] is not changed, then it is a prime
+		if (prime[p] == false)
+		{
+			// Update all multiples of p
+			for (int i=p*2; i<=n; i += p)
+				prime[i] = true;
+		}
+	}
+
+	// Print all prime numbers
+	for (int p=2; p<=n; p++)
+	if (!prime[p])
+		cout << p << " ";
+}
+
+
+
+
+
+
+
+
+
+
 void sieve(int num)
 {
     long long sum = 0;
