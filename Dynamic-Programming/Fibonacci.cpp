73a1c965da964cd8068e004f6f1c6bd174820949
Rashed <rashedcs@users.noreply.github.com>
2016-12-15 10:52:47 +0600

    Create Fibonacci.cpp

---
+++ Dynamic-Programming/Fibonacci.cpp
@@ -0,0 +1,22 @@
+//Top down approach
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, memo[100];
+
+int fibo(int n)
+{
+  if(n<=2)               return 1;
+  else  if(memo[n]!=-1)  return memo[n];
+  else                   return memo[n] = fibo(n-1) + fibo(n-2);
+
+}
+
+
+int main()
+{
+     memset(memo, -1, sizeof (memo));
+     cin>>n;
+     cout<<fibo(n)<<endl;
+	 return 0;
+}
