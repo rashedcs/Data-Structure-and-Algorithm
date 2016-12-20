8a3810574c8f7111fe4d6113052af728deaf1998
Rashed <rashedcs@users.noreply.github.com>
2016-12-20 20:11:04 +0600

    Add files via upload

---
+++ Wrong-Answer/aish.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+    int fib (int n)
+    {
+      cout << "Processing fib(" << n << ")... ";
+
+      if (n < 3 )
+      {
+         cout << "Return 1!\n";
+         return (1);
+      }
+      else
+      {
+       cout << "Call fib(" << n-2 << ") and fib(" << n-1 << ").\n";
+       return fib(n-2) + fib(n-1);
+      }
+   }
+
+
+    int main()
+    {
+
+      int n, answer;
+      cout << "Enter number to find: ";
+      cin >> n;
+
+      cout << "\n\n";
+
+      answer = fib(n);
+      cout << answer << " is the " << n << "th Fibonacci number\n";
+         return 0;
+    }
+
+
+
+
