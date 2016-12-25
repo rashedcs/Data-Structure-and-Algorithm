8d44af1cc63f73a0bd7521e626cd6f23905ec6d5
Rashed <rashedcs@users.noreply.github.com>
2016-12-25 12:19:24 +0600

    Update 0-1 Knapsack.cpp

--- Programming Paradigms/Dynamic Programming/0-1 Knapsack.cpp
+++ Programming Paradigms/Dynamic Programming/0-1 Knapsack.cpp
@@ -114,3 +114,39 @@ int main()
 	}
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+tatic int[] values = new int[] {894, 260, 392, 281, 27};
+static int[] weights = new int[] {8, 6, 4, 0, 21};
+static int W = 30;
+
+private static int knapsack(int i, int W) {
+    if (i < 0) {
+        return 0;
+    }
+    if (weights[i] > W) {
+        return knapsack(i-1, W);
+    } else {
+        return Math.max(knapsack(i-1, W), knapsack(i-1, W - weights[i]) + values[i]);
+    }
+}
+
+public static void main(String[] args) {
+System.out.println(knapsack(values.length - 1, W));}
+
+*/
