e820d6be2abc73fdbc7fa5d631f1d6b613d41ce2
Rashed <rashedcs@users.noreply.github.com>
2016-12-27 00:08:48 +0600

    Update BigMod.cpp

--- Math/4.Number-Theory/BigMod.cpp
+++ Math/4.Number-Theory/BigMod.cpp
@@ -1,13 +1,27 @@
-
-
-
 //(a ^ b) % m
+
+
+//Complexity : O(n)
+int mod(int a, int b, int c)
+{
+    if(b==0) return 1;
+    
+    if(b%2==0)
+    {
+        int x=mod(a,b/2,c);
+        return (x*x)%c;
+    }
+    else return (a%c * mod(a,b-1,c))%c;
+}
+
+
+
 //Complexity : O(log (b))
-template <typename T>T power(T e, T n, T m) 
+template <typename T> T power(T e, T n, T m) 
 {
  	 T x = 1, p = e;
 	 while(n) 
-   {
+         {
 	  	if(n & 1) x = mul(x, p, m);
 	  	p = mul(p, p, m);
 	  	n >>= 1;
