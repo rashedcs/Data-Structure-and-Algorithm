699db78f9972ffddbec5bdb47ec6c4c85a0a7724
Rashed <rashedcs@users.noreply.github.com>
2016-12-28 21:44:23 +0600

    Update LIS.cpp

--- Programming Paradigms/Dynamic Programming/LIS.cpp
+++ Programming Paradigms/Dynamic Programming/LIS.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 int main()
 {
-   vector<int>v;
-   int n, num;
-   cin>>n;
-   for(int i=0; i<n; i++)
-   {
-     cin>>num;
-     vector<int>::iterator it = lower_bound(v.begin(), v.end(), num);
-     if(v.end()==it) v.push_back(num);
-     else   v[it-v.begin()] = num;
-   }
-  cout<<"LIS = "<<v.size()<<endl;
+    vector<int>v;
+    int n, num;
+   
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+      cin>>num;
+      vector<int>::iterator it = lower_bound(v.begin(), v.end(), num);
+      if(v.end()==it) v.push_back(num);
+      else   v[it-v.begin()] = num;
+    }
+    cout<<"LIS = "<<v.size()<<endl;
 
-
-  return 0;
+    return 0;
 }
 
 
