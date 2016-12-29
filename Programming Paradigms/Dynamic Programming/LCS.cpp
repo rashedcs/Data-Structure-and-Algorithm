8a9094acaec0c9d52199b3f4f5ed148b2f36c9af
Rashed <rashedcs@users.noreply.github.com>
2016-12-29 15:43:33 +0600

    Update LCS.cpp

--- Programming Paradigms/Dynamic Programming/LCS.cpp
+++ Programming Paradigms/Dynamic Programming/LCS.cpp
@@ -17,9 +17,9 @@ int solve(int m,int n)
     {
         for(int j=0;j<=n;j++)
         {
-            if(i==0||j==0) dp[i][j]=0;
-            else if(A[i-1]==B[j-1]) dp[i][j]=1+dp[i-1][j-1];
-            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            if(i==0||j==0)            dp[i][j]=0;
+            else if(A[i-1]==B[j-1])   dp[i][j]=1+dp[i-1][j-1];
+            else                      dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
         }
     }
     return dp[m][n];
