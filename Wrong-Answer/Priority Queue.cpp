1e5cb63462710d925306c1e6c998b654b65adb07
Rashed <rashedcs@users.noreply.github.com>
2017-01-26 17:41:15 +0600

    Update Priority Queue.cpp

--- Wrong-Answer/Priority Queue.cpp
+++ Wrong-Answer/Priority Queue.cpp
@@ -18,22 +18,21 @@ int main(void) {
 	return 0;
 }
 
-
-
 #include <bits/stdc++.h>
 using namespace std;
- 
-class Comp
+
+struct comp
 {
-public:
-	bool operator () (int lhs, int rhs) {
+	bool operator () (int lhs, int rhs)
+	{
 		return lhs > rhs;
 	}
 };
- 
-int main(void) {
-	priority_queue<int, vector<int>, Comp> pq;
-	
+
+int main(void)
+{
+	priority_queue<int, vector<int>, comp> pq;
+
 	pq.push(40);
 	pq.push(320);
 	pq.push(42);
@@ -41,7 +40,8 @@ int main(void) {
 	pq.push(65);
 	pq.push(12);
 	pq.push(245);
- 
+
 	cout<<pq.top()<<endl;
 	return 0;
 }
+
