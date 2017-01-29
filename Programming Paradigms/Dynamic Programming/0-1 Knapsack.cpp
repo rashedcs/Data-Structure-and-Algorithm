2cc6e1ec0cae78c54ec663b807daf43b66d71323
Rashed <rashedcs@users.noreply.github.com>
2017-01-29 21:09:55 +0600

    Update 0-1 Knapsack.cpp

--- Programming Paradigms/Dynamic Programming/0-1 Knapsack.cpp
+++ Programming Paradigms/Dynamic Programming/0-1 Knapsack.cpp
@@ -49,50 +49,40 @@ using namespace std;
 
 int knapSack(int wt[], int val[], int n, int W)
 {
-    int K[n+1][W+1];
-    for(int i = 0; i <= n; i++)
-    {
-       for(int w = 0; w <= W; w++)
-       {
-           if (i==0 || w==0)        K[i][w] = 0;
-	       
-           else if (wt[i-1] <= w)   K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
-	       
-           else                     K[i][w] = K[i-1][w];
-       }
+      int dp[n+1][W+1];
+
+      for(int i=0; i<=n; i++)
+      {
+         for(int m=0; m<= W; m++)
+         {
+             if (i==0 || m==0)        dp[i][m] = 0;
+
+             else if (wt[i-1] <= m)   dp[i][m] = max(val[i-1] + dp[i-1][m-wt[i-1]],  dp[i-1][m]);
+
+             else                     dp[i][m] = dp[i-1][m];
+         }
+      }
+     return dp[n][W];
+}
+
+
+   int main()
+   {
+	    int n,W,*va,*wt;
+
+	    cin>>W>>n;
+
+	    va = new int[n];
+
+	    wt = new int[n];
+
+	    for(int i=0;i<n;i++)   cin>>va[i]>>wt[i];
+
+	    cout<<knapSack(wt, va, n, W)<<endl;
+
+
+	    return 0;
     }
-   return K[n][W];
-}
-
-
-int main()
-{
-	int tc,n,W,*v,*w;
-	cin>>tc;
-	while(tc--)
-	{
-	    cin>>n;
-
-	    cin>>W;
-
-	    v = new int[n];
-
-	    w = new int[n];
-
-	    for(int i=0;i<n;i++)   cin>>v[i];
-
-	    for(int i=0;i<n;i++)   cin>>w[i];
-
-	    cout<<knapSack(w, v, n, W)<<endl;
-
-	}
-	return 0;
-}
-
-
-
-
-
 
 
 
@@ -103,32 +93,17 @@ int main()
 
 
 //Please : Do not follow below code......It is just daught code not absolute.
-//Daught : 
-//Source : 
+//Daught
 
-/*
-Source : competitive programming 3
+
+
+///Source : Internet
+
 
 #define MAX_N 2000
 #define MAX_W 2000
 
 int   N, V[MAX_N], W[MAX_N], memo[MAX_N][MAX_W];
-
-int value(int id, int w)
-{
-  if (id == N || w == 0)       return 0; //Base Case
-  else if (memo[id][w] != -1)  return memo[id][w];
-  else  if (W[id] >w)          return memo[id][w] = value(id + 1, w);
-  else                         return memo[id][w] = max(value(id + 1, w),  V[id] + value(id + 1, w - W[id]));
-
-}
-
-
-
-
-
-Source : Internet
-
 int value(int id, int w)
 {
   if (id == N || w == 0)       return 0;
@@ -142,8 +117,7 @@ int value(int id, int w)
   }
 }
 
-Source : Internet
-
+//Source : Internet
 int knapSack(int n, int w)
 {
    if (memo[n][w] != -1)       return memo[n][w];
