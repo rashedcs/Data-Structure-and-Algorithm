bafd33caf10125cfc544e1c8df6bc378871ef564
Rashed <rashedcs@users.noreply.github.com>
2017-02-09 21:24:02 +0600

    Update Intervel Partitioning.cpp

--- Programming Paradigms/Greedy/Intervel Partitioning.cpp
+++ Programming Paradigms/Greedy/Intervel Partitioning.cpp
@@ -1,2 +1,4 @@
 https://tonycao.gitbooks.io/leetcode-locked/content/LeetCode%20Locked/c1.6.html
 http://www.programcreek.com/2014/05/leetcode-meeting-rooms-ii-java/
+https://discuss.leetcode.com/topic/20906/java-ac-code-using-comparator
+https://discuss.leetcode.com/topic/20906/java-ac-code-using-comparator
