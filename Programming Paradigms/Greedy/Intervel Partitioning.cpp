4c046e7265227d48a21dcbf7291a793e768b2368
Rashed <rashedcs@users.noreply.github.com>
2017-02-09 21:45:45 +0600

    Update Intervel Partitioning.cpp

--- Programming Paradigms/Greedy/Intervel Partitioning.cpp
+++ Programming Paradigms/Greedy/Intervel Partitioning.cpp
@@ -3,3 +3,22 @@ http://www.programcreek.com/2014/05/leetcode-meeting-rooms-ii-java/
 https://discuss.leetcode.com/topic/20906/java-ac-code-using-comparator
 http://www.cnblogs.com/grandyang/p/5244720.html
 http://massivealgorithms.blogspot.com/2015/08/like-coding-leetcode-253-meeting-rooms.html
+    public int minMeetingRooms(Interval[] intervals) {
+        int[] starts = new int[intervals.length];
+        int[] ends = new int[intervals.length];
+        for(int i=0; i<intervals.length; i++) {
+            starts[i] = intervals[i].start;
+            ends[i] = intervals[i].end;
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int rooms = 0;
+        int endsItr = 0;
+        for(int i=0; i<starts.length; i++) {
+            if(starts[i]<ends[endsItr])
+                rooms++;
+            else
+                endsItr++;
+        }
+        return rooms;
+    }
