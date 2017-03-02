ab974a7fb5ba35cdf6f50962f0b84ec5fb164b6f
Rashed <rashedcs@users.noreply.github.com>
2017-03-02 18:04:56 +0600

    Add files via upload

---
+++ Computational Geometry/EuclideanDistance.cpp
@@ -0,0 +1,25 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+
+
+
+ double dist(pair<int,int> &p,pair<int,int> &q)
+ {
+    return hypot(p.first-q.first, p.second - q.second);
+    //hypot (dx, dy) returns sqrt(dx*dx, dy*dy)
+ }
+
+
+ int main()
+ {
+	vector<pair<int,int> >V;
+	V.push_back(make_pair(4,3));
+	V.push_back(make_pair(2,2));
+
+    cout<<dist(V[0], V[1])<<endl;
+
+	return 0;
+ }
+
+
+
