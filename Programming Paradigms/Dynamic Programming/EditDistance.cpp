d34ecdca898d5cbc7955764a11f80a0acd882d2c
Rashed <rashedcs@users.noreply.github.com>
2017-03-24 22:40:43 +0600

    Update EditDistance.cpp

--- Programming Paradigms/Dynamic Programming/EditDistance.cpp
+++ Programming Paradigms/Dynamic Programming/EditDistance.cpp
@@ -1,3 +1,55 @@
+//Bottom up approach
+// Edit Distance Dynamic Programing
+// Make a string to another string by adding , removing or replacing minimum number of characters
+// Tabulation Technique
+// Time Complexity O(N*M)
+// N -> 1st string length M -> 2nd string length
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+using namespace std;
+char A[2000+10],B[2000+10];
+int dp[2000+10][2000+10];
+int solve(int len1,int len2)
+{
+    for(int i=1;i<=len1;i++) dp[i][0]=i;
+    for(int i=1;i<=len2;i++) dp[0][i]=i;
+
+    for(int i=1;i<=len1;i++)
+    {
+        for(int j=1;j<=len2;j++)
+        {
+            if(A[i-1]==B[j-1]) dp[i][j] = dp[i-1][j-1];
+            else dp[i][j]=1+min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
+        }
+    }
+    return dp[len1][len2];
+}
+int main()
+{
+    scanf("%s%s",&A,&B);
+    printf("%d\n",solve(strlen(A),strlen(B)));
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Top down approach
 
 #include<bits/stdc++.h>
