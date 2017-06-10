3fb5e3dd5a490348d704465caef49efa060fdc58
Rashed <rashedcs@users.noreply.github.com>
2017-06-10 12:39:14 +0600

    Create reverse bit.cpp

--- Bit Manipulation/reverse bit.cpp
+++ Bit Manipulation/reverse bit.cpp
@@ -6,22 +6,21 @@ int main()
 {
    ios::sync_with_stdio(false);
 
-	int tc, n, bit;
+	int  n, bit;
 	unsigned int rev;
-	cin>>tc;
-	while(tc--)
-	{
-	    bit = 32;
-	    cin>>n;
-        rev=0;
-        for(int i=0; i<bit; i++)
-        {
-            rev<<=1;
-            rev |= (n&1); // // if(n&1)  rev|=1;
-            n>>=1;
-        }
-        cout<<rev<<endl;
-    }
+
+	        bit = 32;
+	        cin>>n;
+		rev=0;
+	
+		for(int i=0; i<bit; i++)
+		{
+		    rev<<=1;
+		    rev |= (n&1); // // if(n&1)  rev|=1;
+		    n>>=1;
+		}
+		cout<<rev<<endl;
+ 
 	return 0;
 }
 
