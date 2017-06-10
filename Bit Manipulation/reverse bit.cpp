1572fe93b958fd301c7b30857c3a9a7d18a58592
Rashed <rashedcs@users.noreply.github.com>
2017-06-10 12:37:34 +0600

    Create reverse bit.cpp

---
+++ Bit Manipulation/reverse bit.cpp
@@ -0,0 +1,31 @@
+//Reverse a 32 bit given number.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   ios::sync_with_stdio(false);
+
+	int tc, n, bit;
+	unsigned int rev;
+	cin>>tc;
+	while(tc--)
+	{
+	    bit = 32;
+	    cin>>n;
+        rev=0;
+        for(int i=0; i<bit; i++)
+        {
+            rev<<=1;
+            rev |= (n&1); // // if(n&1)  rev|=1;
+            n>>=1;
+        }
+        cout<<rev<<endl;
+    }
+	return 0;
+}
+
+
+
+//Reverse the binary number 
+http://algorithms.tutorialhorizon.com/reverse-the-binary-representation-of-a-number/
