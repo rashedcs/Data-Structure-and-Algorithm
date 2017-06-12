5fb78c6641fa67294f5638d043c60734dec41cbe
Rashed <rashedcs@users.noreply.github.com>
2017-06-12 06:48:23 +0600

    Create Graph representation.cpp

--- Graph/Graph representation.cpp
+++ Graph/Graph representation.cpp
@@ -81,3 +81,53 @@ adjacency matrix :
     }
     
     
+Weighted Graph :
+The idea is to use a vector of pair vectors. Below code implements the same.
+
+// C++ program to represent undirected and weighted graph
+// using STL. The program basically prints adjacency list
+// representation of graph
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// To add an edge
+void addEdge(vector <pair<int, int> > adj[], int u,
+                                     int v, int wt)
+{
+    adj[u].push_back(make_pair(v, wt));
+    adj[v].push_back(make_pair(u, wt));
+}
+ 
+// Print adjacency list representaion ot graph
+void printGraph(vector<pair<int,int> > adj[], int V)
+{
+    int v, w;
+    for (int u = 0; u < V; u++)
+    {
+        cout << "Node " << u << " makes an edge with \n";
+        for (auto it = adj[u].begin(); it!=adj[u].end(); it++)
+        {
+            v = it->first;
+            w = it->second;
+            cout << "\tNode " << v << " with edge weight ="
+                 << w << "\n";
+        }
+        cout << "\n";
+    }
+}
+ 
+// Driver code
+int main()
+{
+    int V = 5;
+    vector<pair<int, ints> > adj[V];
+    addEdge(adj, 0, 1, 10);
+    addEdge(adj, 0, 4, 20);
+    addEdge(adj, 1, 2, 30);
+    addEdge(adj, 1, 3, 40);
+    addEdge(adj, 1, 4, 50);
+    addEdge(adj, 2, 3, 60);
+    addEdge(adj, 3, 4, 70);
+    printGraph(adj, V);
+    return 0;
+}
