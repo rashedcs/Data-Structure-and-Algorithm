4714f688824cb9e874e7579aa03e9f2e4ab6edaa
Rashed <rashedcs@users.noreply.github.com>
2017-07-04 19:13:32 +0600

    Update 2d vector.cpp

--- 2d vector.cpp
+++ 2d vector.cpp
@@ -48,3 +48,36 @@ for(int i=0;i<3;i++){
 return 0;
  
 }
+
+
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+ int n, m, i, j;
+vector <vector<int>> v;
+cin>>n>>m; //n is rows and m is columns
+v.resize(n,vector<int>(m));
+for(i=0;i<n;i++)      // inserts elements into the vector v
+ for(j=0;j<m;j++)
+  cin>>v[i][j];
+
+for(i=0;i<n;i++)      //accesses elements of vector v
+ for(j=0;j<m;j++)
+   cout<<v[i][j]<<" ";
+    return 0;
+}
+
+
+http://www.geeksforgeeks.org/sorting-2d-vector-in-c-set-1-by-row-and-column/
+
+
