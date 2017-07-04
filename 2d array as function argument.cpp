f4c648c22189f5410f3a1b28b1ff99759f870461
Rashed <rashedcs@users.noreply.github.com>
2017-07-04 17:24:10 +0600

    Update 2d array as function argument.cpp

--- 2d array as function argument.cpp
+++ 2d array as function argument.cpp
@@ -31,3 +31,92 @@ int main()
 
     doit(arr, n, m);
 }
+
+
+
+
+
+
+ #include<bits/stdc++.h>
+ using namespace std;
+
+
+template<size_t row, size_t col>
+void print(int (&a)[row][col])
+{
+  for(size_t i=0; i<row; i++)
+  {
+     for(size_t j=0; j<col; j++)
+     {
+        cout<<a[i][j]<<" ";
+     }
+     cout<<endl;
+  }
+}
+
+int main()
+{
+   int r, c;
+   cin>>r>>c;
+   int arr[r][c];
+
+  for(size_t i=0; i<r; i++)
+  {
+     for(size_t j=0; j<c; j++)
+     {
+        cin>>arr[i][j];
+     }
+  }
+   print(arr);
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ #include<iostream>
+ using namespace std;
+
+
+template<size_t N, size_t M>
+void print(int (&a)[N][M])
+{
+  for(size_t i=0; i<N; i++)
+  {
+     for(size_t j=0; j<M; j++)
+     {
+        cout<<a[i][j]<<" ";
+     }
+     cout<<endl;
+  }
+}
+
+int main()
+{
+   	int a[2][3] = {
+   	               {1,2,3},
+			       {4,5,6},
+                  };
+print(a);
+
+}
+
+
+*/
