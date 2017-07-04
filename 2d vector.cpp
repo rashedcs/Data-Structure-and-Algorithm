e4d6c52582fe5a49a39b2105fbae263f28141b07
Rashed <rashedcs@users.noreply.github.com>
2017-07-04 19:08:01 +0600

    Create 2d vector.cpp

---
+++ 2d vector.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+ 
+#include<vector>
+ 
+#include<iomanip>
+ 
+using namespace std;
+ 
+int main()
+ 
+{
+ 
+// declaring vector of vectors here
+ 
+vector<vector<int> > items;
+ 
+int k;
+ 
+// i value stands for no of rows here(manipulate i as you like)
+ 
+for (int i =0;i<3;i++)
+ 
+{
+ 
+//adding rows of vectors here
+ 
+items.push_back(vector<int>());
+ 
+//inputting each value into a particular of column size 4
+ 
+for (int j=0;j<4;j++){
+ cin >> k;
+ 
+items[i].push_back(k);}
+ 
+}
+ 
+//printing out the elements
+for(int i=0;i<3;i++){
+		for(int j=0;j<4;j++){
+			cout<< items[i][j]<<"\t\t";
+			
+		}cout<<endl;
+	}
+return 0;
+ 
+}
