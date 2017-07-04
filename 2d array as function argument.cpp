3b38968a3565b6eb14fc29a12a72d286a5a7ab88
Rashed <rashedcs@users.noreply.github.com>
2017-07-04 20:02:53 +0600

    Update 2d array as function argument.cpp

--- 2d array as function argument.cpp
+++ 2d array as function argument.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 void doit(int **s , int row, int col)
 {
     int i, j;
@@ -34,63 +33,6 @@ int main()
 
 
 
-
-
-
- #include<bits/stdc++.h>
- using namespace std;
-
-
-template<size_t row, size_t col>
-void print(int (&a)[row][col])
-{
-  for(size_t i=0; i<row; i++)
-  {
-     for(size_t j=0; j<col; j++)
-     {
-        cout<<a[i][j]<<" ";
-     }
-     cout<<endl;
-  }
-}
-
-int main()
-{
-   int r, c;
-   cin>>r>>c;
-   int arr[r][c];
-
-  for(size_t i=0; i<r; i++)
-  {
-     for(size_t j=0; j<c; j++)
-     {
-        cin>>arr[i][j];
-     }
-  }
-   print(arr);
-
-   return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
  #include<iostream>
  using namespace std;
 
@@ -98,25 +40,28 @@ int main()
 template<size_t N, size_t M>
 void print(int (&a)[N][M])
 {
-  for(size_t i=0; i<N; i++)
-  {
-     for(size_t j=0; j<M; j++)
-     {
-        cout<<a[i][j]<<" ";
-     }
-     cout<<endl;
-  }
+   for(size_t i=0; i<N; i++)
+   {
+      for(size_t j=0; j<M; j++)
+      {
+         cout<<a[i][j]<<" ";
+      }
+      cout<<endl;
+   }
 }
 
 int main()
 {
    	int a[2][3] = {
-   	               {1,2,3},
-			       {4,5,6},
-                  };
-print(a);
+   	                 {1,2,3},
+	                 {4,5,6},
+                     };
+         print(a);
 
 }
 
 
-*/
+Daught : http://ideone.com/dLkB42   x not possible
+Daught : http://ideone.com/GrGJ52   x not possible
+
+
