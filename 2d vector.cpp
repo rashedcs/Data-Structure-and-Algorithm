f3b267e2b19e62527b6d9a71047389056c830323
Rashed <rashedcs@users.noreply.github.com>
2017-07-04 19:54:40 +0600

    Update 2d vector.cpp

--- 2d vector.cpp
+++ 2d vector.cpp
@@ -1,53 +1,49 @@
 https://www.quora.com/How-do-I-create-a-2D-vector-array-using-std-vector
 
 
-#include<iostream>
- 
-#include<vector>
- 
-#include<iomanip>
- 
+#include<bits/stdc++.h>
 using namespace std;
- 
+
+
+void print(const vector <vector<int>> &v, int row, int col)
+{
+    for(int i=0; i<row;  i++)
+    {
+       for(int j=0; j<col; j++)
+       {
+          cout<<v[i][j]<<" ";
+       }
+       cout<<endl;
+    }
+}
+
+
+
 int main()
- 
 {
- 
-// declaring vector of vectors here
- 
-vector<vector<int> > items;
- 
-int k;
- 
-// i value stands for no of rows here(manipulate i as you like)
- 
-for (int i =0;i<3;i++)
- 
-{
- 
-//adding rows of vectors here
- 
-items.push_back(vector<int>());
- 
-//inputting each value into a particular of column size 4
- 
-for (int j=0;j<4;j++){
- cin >> k;
- 
-items[i].push_back(k);}
- 
+    ios::sync_with_stdio(false);
+    int row, col;
+    cin>>row>>col;
+
+    vector <vector<int>> v;
+    v.resize(col,vector<int>(row));
+
+    //v = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+    /** input from use **/
+    for(int i=0; i<row; i++)
+    {
+       for(int j=0; j<col; j++)
+       {
+          cin>>v[i][j];
+       }
+    }
+
+    print(v, row, col);
+
+    return 0;
 }
- 
-//printing out the elements
-for(int i=0;i<3;i++){
-		for(int j=0;j<4;j++){
-			cout<< items[i][j]<<"\t\t";
-			
-		}cout<<endl;
-	}
-return 0;
- 
-}
+
 
 
 
@@ -60,20 +56,33 @@ return 0;
 #include<bits/stdc++.h>
 using namespace std;
 
-
 int main()
 {
- int n, m, i, j;
-vector <vector<int>> v;
-cin>>n>>m; //n is rows and m is columns
-v.resize(n,vector<int>(m));
-for(i=0;i<n;i++)      // inserts elements into the vector v
- for(j=0;j<m;j++)
-  cin>>v[i][j];
+    ios::sync_with_stdio(false);
+    int row, col;
+    cin>>row>>col;
 
-for(i=0;i<n;i++)      //accesses elements of vector v
- for(j=0;j<m;j++)
-   cout<<v[i][j]<<" ";
+    vector <vector<int>> v;
+    v.resize(col,vector<int>(row));
+
+    //v = {{1,2,3}, {4,5,6}, {7,8,9}}; 
+
+    /** input from use **/
+    for(int i=0; i<row; i++)
+    {
+       for(int j=0; j<col; j++)
+       {
+          cin>>v[i][j];
+       }
+    }
+
+    for(int i=0;i<row; i++)
+    {
+       for(int j=0;j<col;j++)
+       {
+          cout<<v[i][j]<<" ";
+       }
+    }
     return 0;
 }
 
