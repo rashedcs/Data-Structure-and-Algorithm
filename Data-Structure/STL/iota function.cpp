d3e1994f1c928ea6204919c5ee60b00bf7b003c3
Rashed <rashedcs@users.noreply.github.com>
2017-07-06 05:59:39 +0600

    Create iota function.cpp

---
+++ Data-Structure/STL/iota function.cpp
@@ -0,0 +1,20 @@
+// C++ code to demonstrate working of iota()
+#include<iostream>
+#include<numeric> // for iota()
+using namespace std;
+int main()
+{
+    // Initializing array with 0 values
+    int ar[6] =  {0};
+ 
+    // Using iota() to assign values
+    iota(ar, ar+6, 20);
+ 
+    // Displaying the new array
+    cout << "The new array after assigning values is : ";
+    for (int i=0; i<6 ; i++)
+       cout << ar[i] << " ";
+ 
+    return 0;
+ 
+}
