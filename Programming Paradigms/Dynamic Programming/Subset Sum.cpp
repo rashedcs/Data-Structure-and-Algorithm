bfd08ec27749663d132057c26fb7b2ec0133344f
Rashed <rashedcs@users.noreply.github.com>
2017-07-05 18:22:48 +0600

    Update Subset Sum.cpp

--- Programming Paradigms/Dynamic Programming/Subset Sum.cpp
+++ Programming Paradigms/Dynamic Programming/Subset Sum.cpp
@@ -1,3 +1,62 @@
+//Bottom up
+#include<bits/stdc++.h>
+using namespace std;
+
+bool issubsetsum(int arr[],int n,int sum)
+{
+	int i,j;
+	bool dp[sum+1][n+1];
+
+	for(i=0;i<=n;i++)   dp[0][i]=true;
+	for(i=1;i<=sum;i++) dp[i][0]=false;
+
+	for(i=1; i<=sum; i++)
+	{
+		for(j=1; j<=n; j++)
+		{
+           dp[i][j] = dp[i][j-1];
+		   if(arr[j-1]<=i)
+		   {
+			  dp[i][j] = dp[i][j] || dp[i-arr[j-1]][j-1];
+		   }
+		}
+	}
+	return dp[sum][n];
+}
+
+
+int main()
+{
+	int t, n, m, i, *arr;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n>>m;
+		arr = new int[n];
+		for(i=0;i<n;i++) cin>>arr[i];
+
+		if(issubsetsum(arr,n,m)==true)
+		cout<<"Yes"<<endl;
+		else
+		cout<<"No"<<endl;
+		free(arr);
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Top down dp
 
 #include<bits/stdc++.h>
