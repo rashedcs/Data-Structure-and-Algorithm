eafc4cb9579da2f9a58389b051a8b994e9b1abc0
Rashed <rashedcs@users.noreply.github.com>
2017-07-06 02:03:11 +0600

    Update Arr.cpp

--- Data-Structure/Array/Arr.cpp
+++ Data-Structure/Array/Arr.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+   int n;
+   cin>>n;
+   int arr[n];
+   for(int i=0; i<n;i++)
+   {
+     cin>>arr[i];
+   }
+
+   for(auto key : arr)
+   {
+      cout<<key<<" ";
+   }
+
+   return 0;
+}
 
