32c31f5cf2ab3676ed96f1a4c045f3326edebf97
Rashed <rashedcs@users.noreply.github.com>
2017-07-06 02:16:55 +0600

    Update Arr.cpp

--- Data-Structure/Array/Arr.cpp
+++ Data-Structure/Array/Arr.cpp
@@ -21,3 +21,29 @@ int main()
    return 0;
 }
 
+
+
+/** Convert array to vector **/
+int arr[3] = {1, 2, 3};
+int n = sizeof x / sizeof x[0]; //array size 
+   below two way:
+     1.  vector<int> v(arr, arr+n);  
+     2.  vector<int> v;   v.assign(arr, arr+n);
+
+
+
+/**Conver vector to array **/
+  vector<int>v = {7, 8, 9, 10, 11};
+  int *arr = v.data();
+
+     svector<int> v;
+     int* a = &v[0];
+
+
+
+
+
+
+
+
+
