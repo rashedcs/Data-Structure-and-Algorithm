b6a0ba47075ac5436e1f5e0d18fb2c2f6bad708d
Rashed <rashedcs@users.noreply.github.com>
2017-07-06 03:59:42 +0600

    Update map.cpp

--- Data-Structure/STL/map.cpp
+++ Data-Structure/STL/map.cpp
@@ -30,43 +30,6 @@
 	}
 
 
-      
-    
-        #include <bits/stdc++.h>
-	using namespace std;
-
-
-
-	int main(int argc, char const *argv[])
-	{
-	        int n, t, maxn;
-	        map<int,int> mp;
-	
-	        cin>>n;
-		for (int i=0; i<n; i++)
-		{
-			cin>>t;
-			mp[t]++;  //Increasing particular index like : 1 2 2 3 then map : 1:1 ; 2:3
-		}
-
-	       for(auto val : mp )
-	       {
-		  int key = val.first;
-		  int value = val.second; ///occurence / value
-		  cout<<key<<" "<<value<<endl;
-	       }
-
-	       for( auto it = my_map.begin(); it != my_map.end(); ++it )
-	       {
-	          int key = it->first;
-	          string& value = it->second;
-	       }
-
-	      return 0;
-	}
-
-
-
 
 
 
@@ -124,3 +87,49 @@
 	map<int, int>m;
 	vector<int>keys;
 	transform( m.begin(), m.end(), back_inserter(keys), [](std::pair<int, int> p) { return p.second;} );
+
+
+
+
+
+
+
+
+
+      
+    /* Find the frequency of array of element */
+        #include <bits/stdc++.h>
+	using namespace std;
+
+
+
+	int main(int argc, char const *argv[])
+	{
+	        int n, t, maxn;
+	        map<int,int> mp;
+	
+	        cin>>n;
+		for (int i=0; i<n; i++)
+		{
+			cin>>t;
+			mp[t]++;  //Increasing particular index like : 1 2 2 3 then map : 1:1 ; 2:3
+		}
+
+	       for(auto val : mp )
+	       {
+		  int key = val.first;
+		  int value = val.second; ///occurence / value
+		  cout<<key<<" "<<value<<endl;
+	       }
+
+	       for( auto it = my_map.begin(); it != my_map.end(); ++it )
+	       {
+	          int key = it->first;
+	          string& value = it->second;
+	       }
+
+	      return 0;
+	}
+
+
+
