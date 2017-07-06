ec0388c13220c6fc2536dc15524e460c8b7c894d
Rashed <rashedcs@users.noreply.github.com>
2017-07-06 17:31:40 +0600

    Update sort.cpp

--- Data-Structure/STL/sort.cpp
+++ Data-Structure/STL/sort.cpp
@@ -36,3 +36,48 @@ int main()
 
     return 0;
 }
+
+
+
+       #include<bits/stdc++.h>
+        using namespace std;
+
+        class Comp
+        {
+          public:
+            int operator()(int a, int b)
+            {
+                return a<b;
+            }
+        };
+
+        int main()
+        {
+            vector<int>v(10);
+
+            for(int i=0; i<10; i++)
+            {
+              cin>>v[i];
+            }
+
+            sort(v.begin(), v.end(), Comp());
+
+            for(auto item : v)
+            {
+              cout<<item<<" ";
+            }
+            cout<<endl;
+            return 0;
+        }
+
+
+
+
+http://fusharblog.com/3-ways-to-define-comparison-functions-in-cpp/
+3 Ways to Define Comparison Functions in C++
+    
+    1. Define operator<()
+    2. Define a custom comparison function
+    3. Define operator()()
+    
+    
