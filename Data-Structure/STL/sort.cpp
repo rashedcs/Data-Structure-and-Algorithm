e10a436ee53faae610bb550757fbf88fe8bda82f
Rashed <rashedcs@users.noreply.github.com>
2017-07-06 13:09:59 +0600

    Update sort.cpp

--- Data-Structure/STL/sort.cpp
+++ Data-Structure/STL/sort.cpp
@@ -1,20 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+/*
 bool comp(int a, int b)
 {
     return a<b;
 }
-
-
+*/
 
 int main()
 {
     int arr[] = { 5, 6, 2, 4, 1, 3 };
 
-    sort(arr, arr+6, cmp);
-
+    //sort(arr, arr+6, cmp);
+    //sort(arr, arr+n, greater<int>());
+    //sort(arr, arr+n, less<int>());
+   
+    /* Acsending order */
+    sort(arr, arr+n);
+    
+    for (auto item : arr)
+    {
+        cout<<item<< " ";
+    }
+   
+    /* Descending order */   
+    sort(arr, arr+n);
+    reverse(arr, ar+n);
+        
     for (auto item : arr)
     {
         cout<<item<< " ";
