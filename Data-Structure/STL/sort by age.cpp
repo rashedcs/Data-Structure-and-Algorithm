24822fd6b2e97cdbab5f8cd2d02caf67e29f1f47
Rashed <rashedcs@users.noreply.github.com>
2017-07-06 07:10:13 +0600

    Update sort by age.cpp

--- Data-Structure/STL/sort by age.cpp
+++ Data-Structure/STL/sort by age.cpp
@@ -31,3 +31,48 @@
             }
             return 0;
         }
+
+
+
+
+
+
+
+
+
+
+        #include<bits/stdc++.h>
+        using namespace std;
+
+
+        struct employee
+        {
+            string name;
+            int age;
+        };
+
+        bool is_younger(employee const& l, employee const& r)
+        {
+            return l.age < r.age;
+        }
+
+        int main()
+        {
+            ios::sync_with_stdio(false);
+
+            employee arr[3];
+
+            for(int j=0; j<3;++j)
+            {
+                cin>>arr[j].name>>arr[j].age;
+            }
+
+            sort(arr, arr+3, is_younger);
+
+            for(int j=0; j<3; j++)
+            {
+                cout<<arr[j].name<<"\t"<<arr[j].age<<"\n";
+            }
+            return 0;
+        }
+
