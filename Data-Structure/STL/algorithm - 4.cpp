c043e98f8c4814a15348fcc44fa16b2518fbfb59
Rashed <rashedcs@users.noreply.github.com>
2017-07-07 12:16:56 +0600

    Update algorithm - 4.cpp

--- Data-Structure/STL/algorithm - 4.cpp
+++ Data-Structure/STL/algorithm - 4.cpp
@@ -40,7 +40,8 @@
         cout<<" \nCopy Back : ";
         vector<int>s = {100, 200, 300};
         vector<int>d(arr, arr+n);
-        copy_backward(s.begin(), s.end()+3, d.begin()+7);
+        copy_backward(s.begin(), s.end(), d.end() );
+        //copy_backward(s.begin(), s.end()+3, d.begin()+7);
         copy(d.begin(), d.end(), ostream_iterator<int>(cout, " "));
         //https://www.tutorialspoint.com/cpp_standard_library/cpp_algorithm_copy_backward.htm
 
