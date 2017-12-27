7221508c32dc75edbf5f7614edcf1d62dc462d8d
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-27 15:09:03 +0600

    codechef nqueen

--- Programming Paradigms/Complete Search/nqueen.cpp
+++ Programming Paradigms/Complete Search/nqueen.cpp
@@ -1,132 +1,54 @@
-/* C/C++ program to solve N Queen Problem using
-   backtracking */
-#define N 8
-#include<bits/stdc++.h>
-
-
-/* A utility function to check if a queen can
-   be placed on board[row][col]. Note that this
-   function is called when "col" queens are
-   already placed in columns from 0 to col -1.
-   So we need to check only left side for
-   attacking queens */
-bool isSafe(int board[N][N], int row, int col)
-{
-    int i, j;
-
-    /* Check this row on left side */
-    for (i = 0; i < col; i++)
-        if (board[row][i])
-            return false;
-
-    /* Check upper diagonal on left side */
-    for (i=row, j=col; i>=0 && j>=0; i--, j--)
-        if (board[i][j])
-            return false;
-
-    /* Check lower diagonal on left side */
-    for (i=row, j=col; j>=0 && i<N; i++, j--)
-        if (board[i][j])
-            return false;
-
-    return true;
-}
-
-/*
-  A recursive utility function to solve N Queen problem
-*/
-bool solveNQUtil(int board[N][N], int col)
-{
-    /* base case: If all queens are placed
-      then return true */
-    if(col >= N)
+  #include<bits/stdc++.h>
+  using namespace std;
+ 
+  int arr[90];
+  void print (int );
+ 
+ 
+  bool place(int k,int i)
+  {
+    for(int j=1;j<=k-1;j++)
+     {
+       if((arr[j]==i)||abs(j-k)==abs(i-arr[j]))  return false;
+     }
+     return true;
+  }
+ 
+ void nqueen(int row, int n)
+  {
+     for(int i=1;i<=n;i++)
+     {
+       if(place(row,i))
+        {
+           arr[row]=i;
+           if(row==n)   print(n);
+           else         nqueen(row+1,n);
+        }
+     }
+  }
+ 
+  void print(int n)
+  {
+    for(int i=1; i<=n; i++)
     {
-       return true;
-    }
-
-    /* Consider this column and try placing
-       this queen in all rows one by one */
-    else
-    {
-      for (int i=0; i<N; i++)
+      for(int j=1; j<=n; j++)
       {
-        /* Check if queen can be placed on
-          board[i][col] */
-         if( isSafe(board, i, col) )
-         {
-            /* Place this queen in board[i][col] */
-            board[i][col] = 1;
-
-            /* recur to place rest of the queens */
-            if(solveNQUtil(board, col + 1)) return true;
-
-            /* If placing queen in board[i][col]
-               doesn't lead to a solution, then
-               remove queen from board[i][col] */
-             board[i][col] = 0; // BACKTRACK
-         }
-       }
-       return false;
+        if(arr[i]==j)  cout<<"Q\t";
+        else cout<<"-\t";
+      }
+      cout<<endl;
     }
-
-     /* If queen can not be place in any row in
-        this colum col  then return false */
-
-}
-
-/* This function solves the N Queen problem using
-   Backtracking. It mainly uses solveNQUtil() to
-   solve the problem. It returns false if queens
-   cannot be placed, otherwise return true and
-   prints placement of queens in the form of 1s.
-   Please note that there may be more than one
-   solutions, this function prints one  of the
-   feasible solutions.*/
-
-
-/* A utility function to print solution */
-void printSolution(int board[N][N])
-{
-    for (int i = 0; i < N; i++)
+    cout<<endl;
+  }
+ 
+  int main()
+  {
+    int n;
+    while(cin>>n)
     {
-        for (int j = 0; j < N; j++)
-            printf(" %d ", board[i][j]);
-        printf("\n");
+      if(n==1)        cout<<"Q\n";
+      else if(n<4)    cout<<"Not Possible\n\n";
+      else            nqueen(1,n);
     }
-}
-
-
-
-bool solveNQ()
-{
-    int board[N][N];
-                    /*
-                    {  {0, 0, 0, 0},
-                       {0, 0, 0, 0},
-                       {0, 0, 0, 0},
-                       {0, 0, 0, 0}
-                     };
-                     */
-
-    memset(board, 0, sizeof board);
-    if(solveNQUtil(board, 0)==false)
-    {
-      printf("Solution does not exist");
-      return false;
-    }
-
-    else
-    {
-      printSolution(board);
-    }
-  //  return true;
-}
-
-
-
-// driver program to test above function
-int main()
-{
-    solveNQ();
     return 0;
-}
+  }
