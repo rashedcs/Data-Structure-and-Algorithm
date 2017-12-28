2c413b20f2944664b115d03930f2b82f63d3494f
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-28 18:45:57 +0600

    string permutation

--- Programming Paradigms/Complete Search/string permutation.cpp
+++ Programming Paradigms/Complete Search/string permutation.cpp
@@ -1,48 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 vector<string>res;
-
-
+ 
+int check(string arr, int m, int n)
+{
+    if(m!=n && arr[m]==arr[n]) return false;
+}
+ 
+ 
 void permute(string arr, int m, int n)
 {
-    if(m>=n)
-    {
-      res.push_back(arr);
-      return;
-    }
-    for(int i=m; i<arr.length(); i++)
-    {
-       swap(arr[i], arr[m]);
-       permute(arr, m+1, n);
-       swap(arr[m], arr[i]); 
-    }
+     if(m>=n)
+     {
+         res.push_back(arr);
+         return;
+     }
+     
+     for(int i=m; i<arr.length(); i++)
+     {
+        if(!check(arr, m, i))
+        {
+             swap(arr[i], arr[m]);
+             permute(arr, m+1, n);
+             swap(arr[m], arr[i]); 
+        }
+     }
 }
-
-
+ 
+  
 int main() 
 {
     int n, tc;
     string str;
-    cin>>tc;
-    
-    while(tc--)
-    {
-        res.clear();
+ 
         cin>>str;
         n = str.length();
         sort(str.begin(), str.end());
         permute(str, 0 ,n);
         sort(res.begin(),res.end());
         
-        for(string iterate:res)
+        for(int i=0; i<res.size(); i++)
         {
-           cout<<iterate<<" ";
+          if(res[i]!=res[i+1]) cout<<res[i]<<endl;
         }
         cout<<endl;
-    }
     return 0;
 }
+
 
 //Extended Program : https://ideone.com/0gwnJk
 //Another Paradigm : https://www.codechef.com/viewsolution/15868684
