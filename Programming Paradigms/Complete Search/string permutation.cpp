8beb5cdc0740f354d7d9bd7113634710c2596707
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-28 16:09:47 +0600

    string permutation

--- Programming Paradigms/Complete Search/string permutation.cpp
+++ Programming Paradigms/Complete Search/string permutation.cpp
@@ -1,33 +1,52 @@
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
- vector<string >v;
-void permu(string s,int l,int h)
+
+vector<string>res;
+
+int check(string arr, int m, int n)
 {
-    if(l==h)
-        v.push_back(s);
-    for(int i=l;i<=h;i++)
-    {
-        swap(s[i],s[l]);
-        permu(s,l+1,h);
-        swap(s[i],s[l]);
-    }
+    if(m!=n && arr[m]==arr[n]) return false;
 }
-int main()
+
+void permute(string arr, int m, int n)
 {
-   // cout << "Hello World!" << endl;
-  
-   string s;
-   int i;
-   cin>>s;
-   permu(s,0,s.size()-1);
-   sort(v.begin(),v.end());
-   for( i=0;i<v.size();i++)
-   {
-       if(v[i]!=v[i+1])
-            cout<<v[i]<<"\n";
-   }
-  // if(v[i]!=v[i-1])
-    //        cout<<v[i]<<"\n";
-    return 0;
+     if(m>=n)
+     {
+         res.push_back(arr);
+         return;
+     }
+     for(int i=m; i<arr.length(); i++)
+     {
+        if(!check(arr, m, i))
+        {
+             swap(arr[i], arr[m]);
+             permute(arr, m+1, n);
+             swap(arr[m], arr[i]); 
+        }
+     }
+}
+
+
+int main() 
+{
+    int n, tc;
+    string str;
+    cin>>tc;
+    
+    while(tc--)
+    {
+        res.clear();
+        cin>>str;
+        n = str.length();
+        sort(str.begin(), str.end());
+        permute(str, 0 ,n);
+        sort(res.begin(),res.end());
+        
+        for(string iterate:res)
+        {
+             cout<<iterate<<" ";
+        }
+        cout<<endl;
+    }
+	  return 0;
 }
