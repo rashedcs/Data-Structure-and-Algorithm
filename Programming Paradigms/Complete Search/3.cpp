f9e7233e2da679fbca012eff7d3fc5be0174b783
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-01-22 11:47:54 +0600

    subset

--- Programming Paradigms/Complete Search/3.cpp
+++ Programming Paradigms/Complete Search/3.cpp
@@ -1,1 +1,48 @@
-leetcode : https://leetcode.com/problems/subsets/description/
+//leetcode : https://leetcode.com/problems/subsets/description/
+//https://ideone.com/8EWDAr
+	#include<bits/stdc++.h>
+        using namespace std;
+
+
+	void print(vector<int>out)
+	{
+
+	 for(int i: out)
+	   cout<<i<<" ";
+	  cout<<endl;
+	}
+
+
+
+	void findpowerSet(int S[], vector<int>&out, int i)
+	{
+		 if(i<0)
+		 {
+		   print(out);
+		   return ;
+		 }
+		 
+		 out.push_back(S[i]);
+		 findpowerSet(S, out, i-1);
+		 out.pop_back();
+		 while(S[i]==S[i-1])
+		   i--;
+
+		findpowerSet(S, out, i-1);
+	}
+
+
+	int main()
+	{
+
+		int S[] = {1, 3, 1};
+		int n = 3;
+		sort(S, S+n);
+		vector<int>out;
+		findpowerSet(S, out, n-1);
+		return 0;
+	}
+
+
+
+
