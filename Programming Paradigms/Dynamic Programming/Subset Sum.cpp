008fd4b273c898ec7a8a51c01a06cc25ad130964
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-09-26 02:20:24 +0600

    Update Subset Sum.cpp

--- Programming Paradigms/Dynamic Programming/Subset Sum.cpp
+++ Programming Paradigms/Dynamic Programming/Subset Sum.cpp
@@ -2,35 +2,33 @@
 using namespace std;
 
 
-bool isSubsetSum(int a[],int n, int s)
+bool isSubsetSum(int a[],int n, int sum)
 {
-    bool dp[n+1][s+1];
-    
-
-    
-    for(int i=0; i<=n; i++)
-    {
-        for(int j=0; j<=s/2; j++)
-        {
-            if(j==0)        
-            {
-                dp[i][j]=1;
-            }
-            else if(i==0 )  
-            {
-                dp[i][j]=0;
-            }
-            else if(a[i-1]>j)
-            {
-                dp[i][j]=dp[i-1][j];
-            }
-            else if(a[i-1]<=j)
-            {
-                dp[i][j]=dp[i-1][j] || dp[i-1][j-a[i-1]];
-            }
-        }
-    }
-    return dp[n][s/2];
+	    bool dp[n+1][s+1];
+	    int s = sum/2;
+	    for(int i=0; i<=n; i++)
+	    {
+		for(int j=0; j<=s; j++)
+		{
+		    if(j==0)        
+		    {
+			dp[i][j]=1;
+		    }
+		    else if(i==0 )  
+		    {
+			dp[i][j]=0;
+		    }
+		    else if(a[i-1]>j)
+		    {
+			dp[i][j]=dp[i-1][j];
+		    }
+		    else if(a[i-1]<=j)
+		    {
+			dp[i][j]=dp[i-1][j] || dp[i-1][j-a[i-1]];
+		    }
+		}
+	    }
+	    return dp[n][s];
 }
 
 int main()
