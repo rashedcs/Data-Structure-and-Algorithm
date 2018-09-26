5f12b68ce827bf338d93596ef1fc27ed1ffbd750
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-09-26 23:51:54 +0600

    Min number of ways

--- Programming Paradigms/Dynamic Programming/Coin Change 2.cpp
+++ Programming Paradigms/Dynamic Programming/Coin Change 2.cpp
@@ -1,6 +1,6 @@
 	int coinChange(vector<int>& coins, int amount)
 	{
-        int max = amount+1; 
+                int max = amount+1; 
 		vector<int> dp(amount+1, max); //fill with max cause it can not exceeds max
         
 		dp[0] = 0;
@@ -8,13 +8,12 @@
 		{
 			for (int j=1; j<=amount; j++)
 			{
-                if(j>=coins[i]) 
-                {
-                    dp[j]=min(dp[j], dp[j-coins[i]]+1);
-                }
+				if(j>=coins[i]) 
+				{
+				    dp[j]=min(dp[j], dp[j-coins[i]]+1);
+				}
 			}
 		}
-        
 		if (dp[amount]<max) return dp[amount];
 		else return -1;
 	}
