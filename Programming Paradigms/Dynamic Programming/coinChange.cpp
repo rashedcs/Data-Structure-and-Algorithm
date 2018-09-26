232216d2715fbf8a6a691d93aa8ea1c9b8f6b018
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-09-26 21:23:06 +0600

    Coin change ways

--- Programming Paradigms/Dynamic Programming/coinChange.cpp
+++ Programming Paradigms/Dynamic Programming/coinChange.cpp
@@ -1,42 +1,34 @@
-//Source : http://www.geeksforgeeks.org/dynamic-programming-set-7-coin-change/
-
 #include<bits/stdc++.h>
 using namespace std;
 
 
-int count( int S[], int m, int n )
+int coinChange(int arr[], int n, int k)
 {
-    // table[i] will be storing the number of solutions for
-    // value i. We need n+1 rows as the table is consturcted
-    // in bottom up manner using the base case (n = 0)
-    int table[n+1];
-
-    // Initialize all table values as 0
-    memset(table, 0, sizeof(table));
-
-    // Base case (If given value is 0)
-    table[0] = 1;
-
-    // Pick all coins one by one and update the table[] values
-    // after the index greater than or equal to the value of the
-    // picked coin
-    for(int i=0; i<m; i++)
-    {
-        for(int j=S[i]; j<=n; j++)
+        int dp[k+1]={};
+        dp[0] = 1;
+        for (int i=0; i<n; i++) 
         {
-            table[j] += table[j-S[i]];
+            for (int j=1; j<=k; j++) 
+            {
+                if(j>=arr[i]) dp[j] += dp[j - arr[i]];
+            }
         }
-    }
-
-    return table[n];
+        return dp[k];
 }
 
-// Driver program to test above function
+
 int main()
 {
-    int arr[] = {1, 2, 3};
-    int m = sizeof(arr) / sizeof(arr[0]);
-    int n = 4;
-    printf(" %d ", count(arr, m, n));
-    return 0;
+    int  n, k;
+
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        cin>>k;
+        cout<<coinChange(arr, n, k)<<endl;
+    
+	   return 0;
 }
