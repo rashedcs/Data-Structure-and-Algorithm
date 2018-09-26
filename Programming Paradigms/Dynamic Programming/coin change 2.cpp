bbafdbaa932397d2e6bcdde3ad14db67fd9c6fe6
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-09-26 23:43:46 +0600

    Create coin change 2.cpp

---
+++ Programming Paradigms/Dynamic Programming/coin change 2.cpp
@@ -0,0 +1,20 @@
+	int coinChange(vector<int>& coins, int amount)
+	{
+        int max = amount+1; 
+		vector<int> dp(amount+1, max); //fill with max cause it can not exceeds max
+        
+		dp[0] = 0;
+		for (int i=0; i<coins.size(); i++)
+		{
+			for (int j=1; j<=amount; j++)
+			{
+                if(j>=coins[i]) 
+                {
+                    dp[j]=min(dp[j], dp[j-coins[i]]+1);
+                }
+			}
+		}
+        
+		if (dp[amount]<max) return dp[amount];
+		else return -1;
+	}
