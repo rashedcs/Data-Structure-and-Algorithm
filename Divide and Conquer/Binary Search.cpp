216db4d2098d5bfb77c1ae0430c0c497ac134b57
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-09-27 14:32:37 +0600

    Binary Search

--- Divide and Conquer/Binary Search.cpp
+++ Divide and Conquer/Binary Search.cpp
@@ -1,31 +1,18 @@
 #include <iostream>
 using namespace std;
  
-// Binary search algorithm to return the position of 
-// target x in the sub-array A[low..high] 
+
 int BinarySearch(int A[], int low, int high, int x)
 {
-    // Base condition (search space is exhausted)
     if (low > high)
         return -1;
  
-    // we find the mid value in the search space and 
-    // compares it with target value
- 
-    int mid = (low + high)/2;    // overflow can happen
-    // int mid = low + (high - low)/2;
-    
-    // Base condition (target value is found)
+    int mid = (low + high)/2;   
+
     if (x == A[mid])
         return mid;
-    
-    // discard all elements in the right search space 
-    // including the mid element     
     else if (x < A[mid])
         return BinarySearch(A, low,  mid - 1, x);
-    
-    // discard all elements in the left search space 
-    // including the mid element
     else 
         return BinarySearch(A, mid + 1,  high, x);
 }
@@ -47,8 +34,8 @@ int main()
     
     return 0;
 }
-/*
 
+/*
 #include <iostream>
 #include <algorithm>
 #include <vector>
