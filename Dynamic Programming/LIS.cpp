f694f53ee5f671ad2d618657e74d54d185de2c06
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-09-27 13:39:56 +0600

    Longest Increasing Subsequence

--- Dynamic Programming/LIS.cpp
+++ Dynamic Programming/LIS.cpp
@@ -1,78 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define mx 100
-int LIS[mx],seq[mx],sol[mx];
-int solve(int N)
+
+int lis(int arr[],int n)
 {
-    for(int i=1;i<=N;i++) LIS[i]=1;
-    int ans=1;
-    for(int i=1;i<=N;i++)
+    int dp[n];
+    for(int i=0; i<n; i++)
     {
-        for(int j=i+1;j<=N;j++)
+             dp[i]=1;
+    }
+    for(int i=1; i<n; i++)
+    {
+        for(int j=0;j<i;j++)
         {
-            if(seq[i]<seq[j]&&LIS[j]<LIS[i]+1)
+            if(arr[i]>arr[j])
             {
-                LIS[j]=LIS[i]+1;
-                ans=max(ans,LIS[j]);
+                dp[i]=max(dp[i], dp[j]+1);
             }
         }
     }
-    return ans;
+    return  *max_element(dp, dp+n);
 }
-void printSolution(int N,int lis)
+
+int main() 
 {
-    int temp = lis;
-    while(N>0)
-    {
-        if(LIS[N]==temp)
-        {
-            sol[temp--]=seq[N];
-        }
-        N--;
-    }
-    for(int i=1;i<=lis;i++) printf("%d ",sol[i]);
-}
-int main()
-{
-    int N; // N is the number of elements
-    scanf("%d",&N);
-    for(int i=1;i<=N;i++) scanf("%d",&seq[i]);
-    int ans = solve(N);
-    printf("%d\n",ans);
-    printSolution(N,ans);
-    return 0;
+	int n;
+	     cin >> n;
+	     int arr[n];
+	     for(int i=0;i<n;i++)
+	     {
+	         cin >> arr[i];
+	     }
+	     cout<<lis(arr,n)<<endl;
+	return 0;
 }
 
 
 
+/*
+Code with 0(nlogn)  : 
+https://ide.geeksforgeeks.org/AsPOG8WSps
+*/
 
-
-
-
-
-
-
-//Complexity : 0(nlogn)
-
-#include<bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-    vector<int>v;
-    int n, num;
-   
-    cin>>n;
-    for(int i=0; i<n; i++)
-    {
-      cin>>num;
-      vector<int>::iterator it = lower_bound(v.begin(), v.end(), num);
-      if(v.end()==it) v.push_back(num);
-      else   v[it-v.begin()] = num;
-    }
-    cout<<"LIS = "<<v.size()<<endl;
-
-    return 0;
-}
 
 
