74cd7319f05338a7344c0433bbffeac265e8c0e5
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-09-27 14:36:23 +0600

    Merge Sort

--- Programming Paradigms/Divide and Conquer/MergeSort.cpp
+++ Programming Paradigms/Divide and Conquer/MergeSort.cpp
@@ -1,6 +1,3 @@
-//Modified Source Code :   http://code.geeksforgeeks.org/yy6559
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -75,81 +72,10 @@ int main()
    return 0;
 }
 
-
-
-
-
-Alternative way :
-
-#include<bits/stdc++.h>
-using namespace std;
-
-#define mx 10
-
-int a[mx],b[mx] ;
-
-void sortion(int low, int high)
-{
-   int i, mid,l1, l2;
-
-   if(low<high)
-   {
-      mid = (low + high) / 2;
-      sortion(low, mid);
-      sortion(mid+1, high);
-
-      l1 = low;
-      l2 = mid + 1;
-      i  = low;
-
-      while(l1<=mid && l2<=high)
-      {
-        if(a[l1] <= a[l2])
-        {
-          b[i++] = a[l1++];
-        }
-
-        else
-        {
-          b[i++] = a[l2++];
-        }
-      }
-
-      while(l1 <= mid)
-      {
-         b[i++] = a[l1++];
-      }
-
-      while(l2<=high)
-      {
-         b[i++] = a[l2++];
-      }
-
-      for(i=low; i<=high; i++)
-      {
-        a[i] = b[i];
-      }
-   }
-}
-
-
-
-// Drive Function
-int main()
-{
-
-   for(int i=1; i<=mx; i++)   cin>>a[i];
-
-   sortion(1, mx);
-
-   for(int k=1; k<=mx; k++)   cout<<a[k]<<" ";
-
-   return 0;
-}
-
-
-
-
+/*
+Link 1 : http://code.geeksforgeeks.org/yy6559
+Link 2 : https://ideone.com/YkWjYG
+*/
 
 
 
