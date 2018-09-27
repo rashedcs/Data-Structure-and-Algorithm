56e7bfcc04658e1c92fab2cf253d4cbe4dc225bb
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-09-27 12:50:42 +0600

    0/1 knapsack

--- Dynamic Programming/0-1 Knapsack.cpp
+++ Dynamic Programming/0-1 Knapsack.cpp
@@ -1,3 +1,50 @@
+
+//Bottom Up Approach
+#include<bits/stdc++.h>
+using namespace std;
+//#define mx 1000
+//int price[mx],weight[mx],dp[mx][mx];
+
+int knapSack(int wt[], int val[], int n, int W)
+{
+      int dp[n+1][W+1];
+
+      for(int i=0; i<=n; i++)
+      {
+         for(int m=0; m<= W; m++)
+         {
+             if (i==0 || m==0)        dp[i][m] = 0;
+
+             else if (wt[i-1] <= m)   dp[i][m] = max(val[i-1] + dp[i-1][m-wt[i-1]],  dp[i-1][m]);
+
+             else                     dp[i][m] = dp[i-1][m];
+         }
+      }
+     return dp[n][W];
+}
+
+
+int main()
+ {
+	    int n,W,*va,*wt;
+
+	    cin>>W>>n;
+
+	    va = new int[n];
+
+	    wt = new int[n];
+
+	    for(int i=0;i<n;i++)   cin>>va[i]>>wt[i];
+
+	    cout<<knapSack(wt, va, n, W)<<endl;
+
+
+	    return 0;
+    }
+
+
+
+
 //Top down approach : 
 #include<bits/stdc++.h>
 using namespace std;
@@ -41,46 +88,3 @@ int main()
 
 
 
-
-//Bottom Up Approach
-#include<bits/stdc++.h>
-using namespace std;
-//#define mx 1000
-//int price[mx],weight[mx],dp[mx][mx];
-
-int knapSack(int wt[], int val[], int n, int W)
-{
-      int dp[n+1][W+1];
-
-      for(int i=0; i<=n; i++)
-      {
-         for(int m=0; m<= W; m++)
-         {
-             if (i==0 || m==0)        dp[i][m] = 0;
-
-             else if (wt[i-1] <= m)   dp[i][m] = max(val[i-1] + dp[i-1][m-wt[i-1]],  dp[i-1][m]);
-
-             else                     dp[i][m] = dp[i-1][m];
-         }
-      }
-     return dp[n][W];
-}
-
-
-   int main()
-   {
-	    int n,W,*va,*wt;
-
-	    cin>>W>>n;
-
-	    va = new int[n];
-
-	    wt = new int[n];
-
-	    for(int i=0;i<n;i++)   cin>>va[i]>>wt[i];
-
-	    cout<<knapSack(wt, va, n, W)<<endl;
-
-
-	    return 0;
-    }
